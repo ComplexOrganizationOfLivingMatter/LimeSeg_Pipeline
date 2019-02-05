0c9c31d12022e381a27747513643859d5cd0001b
Pablo Vicente Munuera <pvicente1@us.es>
2019-02-05 10:55:23 +0100

    bugfix and adding more debugging variables

--- src/connectVerticesOf2D.m
+++ src/connectVerticesOf2D.m
@@ -2,6 +2,13 @@ function connectVerticesOf2D(midSectionImage, neighbours2D, vertices2D, vertices
 %CONNECTVERTICESOF2D Summary of this function goes here
 %   Detailed explanation goes here
     
+    for numVertex = 1:size(vertices2D, 1)
+        actualEdges = cellfun(@(x) length(intersect(x, neighbours2D(numVertex, :))), mat2cell(neighbours2D, ones(1, size(neighbours2D, 1)), 3));
+        edgesOfVertex{numVertex, 1} = vertices2D(actualEdges>1, :);
+        edgesOfVertex{numVertex, 2} = neighbours2D(actualEdges>1, :);
+        edgesOfVertex{numVertex, 3} = actualEdges(actualEdges>1);
+    end
+
     for numCentroid = 1:size(centroids, 1)
         numCell = midSectionImage(midSectionNewLabels == numCentroid);
         numCell = numCell(1);
@@ -19,10 +26,11 @@ function connectVerticesOf2D(midSectionImage, neighbours2D, vertices2D, vertices
         end
         
         allActualVertices = [vertices2D(actualVertices, :); vertices2D_Left(actualVertices, :); vertices2D_Right(actualVertices, :)];
-        
+        actualNeighbours2D = [neighbours2D(actualVertices, :); neighbours2D(actualVertices, :); neighbours2D(actualVertices, :)];
         [yValidRegion, xValidRegion] = find(imdilate(actualImg, strel('disk', 10)));
         
         %Get only the closest vertices to the captured region
+        actualNeighbours2D(ismember(allActualVertices, [xValidRegion, yValidRegion], 'rows') == 0, :) = [];
         allActualVertices(ismember(allActualVertices, [xValidRegion, yValidRegion], 'rows') == 0, :) = [];
         
         [~, closestIndices] = pdist2(centroids3x, allActualVertices, 'euclidean', 'Smallest', 1);
@@ -42,8 +50,20 @@ function connectVerticesOf2D(midSectionImage, neighbours2D, vertices2D, vertices
 %                 end
 %             end
 %         end
+        selectedNeighboursOfVertices = actualNeighbours2D(closestIndices == midCentroid, :);
+        if size(unique(selectedNeighboursOfVertices, 'rows'), 1) ~= size(allActualVertices(closestIndices == midCentroid, :), 1)
+            %Wrong vertices
+            
+            % Remove pixels that share neighbours 
+            % The ideal case would be the one in which the same vertex is
+            % shared with other cells
+            numCell
+            [newVertOrder] = boundaryOfCell(allActualVertices(closestIndices == midCentroid, :), centroids(numCentroid, :));
+        else
+            [newVertOrder] = boundaryOfCell(allActualVertices(closestIndices == midCentroid, :), centroids(numCentroid, :));
+        end
         
-        [newVertOrder] = boundaryOfCell(allActualVertices(closestIndices == midCentroid, :), centroids(numCentroid, :));
+        %
         [newOrderX, newOrderY] = poly2cw(newVertOrder((1:end-1), 1), newVertOrder((1:end-1), 2));
         verticesRadius = [];
         
