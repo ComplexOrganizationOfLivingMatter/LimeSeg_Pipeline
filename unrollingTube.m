9495aad78b1a7e0b58b496e034996508f3cf6289
Pablo Vicente Munuera <pvicente1@us.es>
2019-02-21 16:56:02 +0100

    removing the wrong part from basal

--- unrollingTube.m
+++ unrollingTube.m
@@ -7,7 +7,46 @@ addpath(genpath('gui'))
 
 files = dir('**/Salivary gland/**/Results/3d_layers_info.mat');
 
-for numFile = 1:length(files)
+% for numFile = 1:length(files)
+%     if contains(lower(files(numFile).folder), 'discarded') == 0
+%         load(fullfile(files(numFile).folder, '3d_layers_info.mat'));
+% 
+%         figure; paint3D(basalLayer, [], colours)
+%         xlabel('x')
+%         ylabel('y')
+%         zlabel('z')
+%         axisToChange = input('axis:');
+%         if isequal(axisToChange, '') == 0
+%             
+%             size(basalLayer)
+%             valuesToChange = input('values to remove:');
+%             if isequal(axisToChange, 'x')
+%                 basalLayer(valuesToChange, :, :) = 0;
+%                 apicalLayer(valuesToChange, :, :) = 0;
+%                 labelledImage(valuesToChange, :, :) = 0;
+%                 lumenImage(valuesToChange, :, :) = 0;
+%             elseif isequal(axisToChange, 'y')
+%                 basalLayer(:, valuesToChange, :) = 0;
+%                 apicalLayer(:, valuesToChange, :) = 0;
+%                 labelledImage(:, valuesToChange, :) = 0;
+%                 lumenImage(:, valuesToChange, :) = 0;
+%             end
+%             
+%             figure; paint3D(basalLayer, [], colours)
+%             xlabel('x')
+%             ylabel('y')
+%             zlabel('z')
+% 
+%             resultOK = input('everithing ok? ');
+%             if isequal(resultOK, 'OK')
+%                 save(fullfile(files(numFile).folder, '3d_layers_info.mat'), 'labelledImage', 'basalLayer', 'apicalLayer', 'apical3dInfo', 'basal3dInfo', 'colours', 'lumenImage','glandOrientation', '-v7.3')
+%             end
+%         end
+%     end
+%     close all
+% end
+
+parfor numFile = 1:length(files)
     if contains(lower(files(numFile).folder), 'discarded') == 0
         files(numFile).folder
         selpath = files(numFile).folder;
