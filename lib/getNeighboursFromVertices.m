4f01f3ac251f85a47d2c5982a2e33fb7cae28a1a
Pablo Vicente Munuera <pvicente1@us.es>
2019-03-19 14:03:29 +0100

    Update getNeighboursFromVertices.m

--- lib/getNeighboursFromVertices.m
+++ lib/getNeighboursFromVertices.m
@@ -1,8 +1,8 @@
 function [neighbours] = getNeighboursFromVertices(verticesNeighbours)
 %GETNEIGHBOURSFROMVERTICES Summary of this function goes here
 %   Detailed explanation goes here
-            neighbours = arrayfun(@(x) unique(verticesNeighbours(any(ismember(verticesNeighbours, x), 2), :)), 1:max(verticesNeighbours(:)), 'UniformOutput', false);
-            neighbours = cellfun(@(x, y) x(x ~= y), neighbours, num2cell(1:length(neighbours)), 'UniformOutput', false);
-            neighbours = cellfun(@(x) x(:), neighbours, 'UniformOutput', false);
+    neighbours = arrayfun(@(x) unique(verticesNeighbours(any(ismember(verticesNeighbours, x), 2), :)), 1:max(verticesNeighbours(:)), 'UniformOutput', false);
+    neighbours = cellfun(@(x, y) x(x ~= y), neighbours, num2cell(1:length(neighbours)), 'UniformOutput', false);
+    neighbours = cellfun(@(x) x(:), neighbours, 'UniformOutput', false);
 end
 
