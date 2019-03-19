643b24f55c0bac3f6eb03c5c1a04252b9b364ad9
Pablo Vicente Munuera <pvicente1@us.es>
2019-03-19 09:59:28 +0100

    Refactor

--- src/getUnrollOfDividedGlands.m
+++ src/getUnrollOfDividedGlands.m
@@ -1,14 +1,3 @@
-
-% files = dir('**/Salivary gland/**/Results/dividedGland/glandDividedInSurfaceRatios.mat');
-% for numFile = 1:length(files)
-%     load(fullfile(files(numFile).folder, 'glandDividedInSurfaceRatios.mat'))
-%     %figure; paint3D( ismember(imageOfSurfaceRatios{numPartition, 3}, validCells) .* imageOfSurfaceRatios{numPartition, 3}, [], colours);
-%     intermidScutoids = sum(infoPerSurfaceRatio{6, 4}.Scutoids) / size(infoPerSurfaceRatio{6, 4}, 1);
-%     finalScutoids = sum(infoPerSurfaceRatio{11, 4}.Scutoids) / size(infoPerSurfaceRatio{11, 4}, 1);
-%     logScutoids(numFile) = intermidScutoids/finalScutoids;
-% end
-
-
 clear all
 close all
 
