a359246924f84583ea66f0f9c73e3dc84b47509b
Pablo Vicente Munuera <pvicente1@us.es>
2019-03-29 14:29:25 +0100

    all working but a bug

--- lib/obtainVerticesOfBorderCells.m
+++ lib/obtainVerticesOfBorderCells.m
@@ -1,4 +1,4 @@
-function [verticesNeighs2D, vertices2D] = obtainVerticesOfBorderCells(deployedImg, deployedImg3x, verticesNeighs2D, vertices2D, borderCells)
+function [verticesNeighs2D, vertices2D] = obtainVerticesOfBorderCells(deployedImg, deployedImg3x, numBorderCell)
 %OBTAINVERTICESOFBORDERCELLS Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,39 +6,36 @@ function [verticesNeighs2D, vertices2D] = obtainVerticesOfBorderCells(deployedIm
     newVertices{2} = [];
     newVerticesNeighs2D{1} = [];
     newVerticesNeighs2D{2} = [];
-    for numBorderCell = borderCells'
-        labelledActualCells3x = bwlabel(deployedImg3x==numBorderCell);
-        newLabelBorderCells = unique(labelledActualCells3x .* ismember(deployedImg, numBorderCell));
-        newLabelBorderCells(newLabelBorderCells==0) = [];
-        
-        cellsToCalculateNeighs = ismember(labelledActualCells3x, newLabelBorderCells);
-        regionToCalculateNeighs = double(imdilate(cellsToCalculateNeighs, strel('disk', 1)));
-        
-        [~, y] = find(regionToCalculateNeighs);
-        indicesMidImage = find(regionToCalculateNeighs);
-        
-        regionToCalculateNeighs(indicesMidImage(y < (size(deployedImg3x, 2) / 2))) = 1;
-        regionToCalculateNeighs(indicesMidImage(y >= (size(deployedImg3x, 2) / 2))) = 2;
-        
-        for numSide = 1:2
-            img_sideOfBorderCell = (regionToCalculateNeighs == numSide) .* deployedImg3x;
-            neighbours = calculateNeighbours(img_sideOfBorderCell);
-            [newVerticesActual] = getVertices(img_sideOfBorderCell, neighbours);
-            newVertices{numSide} = [newVertices{numSide}; vertcat(newVerticesActual.verticesPerCell{:})];
-            newVerticesNeighs2D{numSide} = [newVerticesNeighs2D{numSide}; newVerticesActual.verticesConnectCells];
-        end
+    labelledActualCells3x = bwlabel(deployedImg3x==numBorderCell);
+    newLabelBorderCells = unique(labelledActualCells3x .* ismember(deployedImg, numBorderCell));
+    newLabelBorderCells(newLabelBorderCells==0) = [];
+
+    cellsToCalculateNeighs = ismember(labelledActualCells3x, newLabelBorderCells);
+    regionToCalculateNeighs = double(imdilate(cellsToCalculateNeighs, strel('disk', 1)));
+
+    [~, y] = find(regionToCalculateNeighs);
+    indicesMidImage = find(regionToCalculateNeighs);
+
+    regionToCalculateNeighs(indicesMidImage(y < (size(deployedImg3x, 2) / 2))) = 1;
+    regionToCalculateNeighs(indicesMidImage(y >= (size(deployedImg3x, 2) / 2))) = 2;
+
+    for numSide = 1:2
+        img_sideOfBorderCell = (regionToCalculateNeighs == numSide) .* deployedImg3x;
+        neighbours = calculateNeighbours(img_sideOfBorderCell);
+        [newVerticesActual] = getVertices(img_sideOfBorderCell, neighbours);
+        %emptyCells = cellfun(@isempty, newVerticesActual.verticesPerCell);
+%         newVertices{numSide} = [newVertices{numSide}; vertcat(newVerticesActual.verticesPerCell{emptyCells==0})];
+%         newVerticesNeighs2D{numSide} = [newVerticesNeighs2D{numSide}; newVerticesActual.verticesConnectCells(emptyCells==0, :)];
+        newVertices{numSide} = [newVertices{numSide}; vertcat(newVerticesActual.verticesPerCell{:})];
+        newVerticesNeighs2D{numSide} = [newVerticesNeighs2D{numSide}; newVerticesActual.verticesConnectCells];
     end
-    
+
     [newVerticesNeighs2D{1}, indicesUnique_1] = unique(newVerticesNeighs2D{1}, 'rows');
     newVertices{1} = newVertices{1}(indicesUnique_1, :);
     [newVerticesNeighs2D{2}, indicesUnique_2] = unique(newVerticesNeighs2D{2}, 'rows');
     newVertices{2} = newVertices{2}(indicesUnique_2, :);
-    
-    verticesToRemove = any(ismember(verticesNeighs2D, borderCells), 2);
-    verticesNeighs2D(verticesToRemove, :) = [];
-    vertices2D(verticesToRemove, :) = [];
-    
-    vertices2D = [vertices2D; newVertices{1}; newVertices{2}];
-    verticesNeighs2D = [verticesNeighs2D; newVerticesNeighs2D{1}; newVerticesNeighs2D{2}];
+
+    vertices2D = [newVertices{1}; newVertices{2}];
+    verticesNeighs2D = [newVerticesNeighs2D{1}; newVerticesNeighs2D{2}];
 end
 
--- src/connectVerticesOf2D.m
+++ src/connectVerticesOf2D.m
@@ -2,14 +2,16 @@ function samiraTable = connectVerticesOf2D(cylindre2DImage, neighbours2D, vertic
 %CONNECTVERTICESOF2D Summary of this function goes here
 %   Detailed explanation goes here
     
-    maxDistance = 4;
+    maxDistance = 50;
 
     midSectionImage_closed = imclose(cylindre2DImage>0, strel('disk', 2));
     sizeRoll = sum(midSectionImage_closed, 2);
     cellVertices = cell(1, max(cylindre2DImage(:)));
+    cellNeighbours = cell(1, max(cylindre2DImage(:)));
     for numCell = 1:max(cylindre2DImage(:))
         actualVertices = any(ismember(neighbours2D, numCell), 2);
         actualCellVertices = vertices2D(actualVertices, :);
+        actualCellNeighbours = neighbours2D(actualVertices, :);
         if ismember(numCell, borderCells)
 %             % Obtaining the vertices of both sides if it is a border cell
 %             changeOfSide = actualCellVertices(:, 2)-(size(cylindre2DImage, 2)/2);
@@ -23,17 +25,21 @@ function samiraTable = connectVerticesOf2D(cylindre2DImage, neighbours2D, vertic
 %             
 %             actualCellVertices = [actualCellVertices; newVertices];
             [actualCellNeighbours, actualCellVertices] = obtainVerticesOfBorderCells(cylindre2DImage, deployedImg3x, numCell);
-            
-            for numCellNeighbour = unique(actualCellNeighbours(:))
-                actualCells = any(ismember(actualCellNeighbours, numCellNeighbour), 2);
-                actualCellVerticesWithSharedNeighbour = actualCellVertices(actualCells, :);
-                verticesOfActualNeighbour = cellVertices{numCellNeighbour};
-                [badVertices, goodVertices] = find(pdist2(actualCellVerticesWithSharedNeighbour, verticesOfActualNeighbour) < maxDistance);
-                actualCellVertices(ismember(actualCellVertices, actualCellVerticesWithSharedNeighbour(badVertices, :), 'rows'), :) = verticesOfActualNeighbour(goodVertices, :);
+        end
+        
+        for numCellNeighbour = unique(actualCellNeighbours(:))'
+            verticesOfActualNeighbour = cellVertices{numCellNeighbour};
+            if isempty(verticesOfActualNeighbour) == 0
+                verticesConnectingActualNeighbour = cellNeighbours{numCellNeighbour};
+                [badVertices, goodVertices] = find(pdist2(actualCellVertices, verticesOfActualNeighbour) < maxDistance);
+                
+                sharingNeighboursVertex = all(arrayfun(@isequal, actualCellNeighbours(badVertices, :), verticesConnectingActualNeighbour(goodVertices, :)), 2);
+                actualCellVertices(badVertices(sharingNeighboursVertex), :) = verticesOfActualNeighbour(goodVertices(sharingNeighboursVertex), :);
             end
         end
         
         cellVertices{numCell} = actualCellVertices;
+        cellNeighbours{numCell} = actualCellNeighbours;
     end
     
     noValidCells = ones(max(cylindre2DImage(:)), 1);
