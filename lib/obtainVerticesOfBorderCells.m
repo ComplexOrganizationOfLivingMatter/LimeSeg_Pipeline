45e6a362bbf1780eada9175ab096b52907661a91
Pablo Vicente Munuera <pvicente1@us.es>
2019-04-08 09:33:39 +0200

    finding missing vertices

--- lib/obtainVerticesOfBorderCells.m
+++ lib/obtainVerticesOfBorderCells.m
@@ -24,14 +24,31 @@ function [verticesNeighs2D, vertices2D] = obtainVerticesOfBorderCells(deployedIm
         neighbours = calculateNeighbours(img_sideOfBorderCell);
         %neighbours = checkPairPointCloudDistanceCurateNeighbours(img3d, neighbours);
         [newVerticesActual] = getVertices(img_sideOfBorderCell, neighbours);
-        emptyCells = any(ismember(newVerticesActual.verticesConnectCells, numBorderCell), 2)==0;
-        %emptyCells = cellfun(@isempty, newVerticesActual.verticesPerCell);
-        newVertices{numSide} = [newVertices{numSide}; vertcat(newVerticesActual.verticesPerCell{emptyCells==0})];
-        newVerticesNeighs2D{numSide} = [newVerticesNeighs2D{numSide}; newVerticesActual.verticesConnectCells(emptyCells==0, :)];
+        neighbourOfBorderCell = any(ismember(newVerticesActual.verticesConnectCells, numBorderCell), 2)==0;
+        emptyCells = cellfun(@isempty, newVerticesActual.verticesPerCell);
+        
+        if any(emptyCells)
+            numRadius = 3;
+            for numEmptyCell = find(emptyCells)'
+                newVertexX = [];
+                while isempty(newVertexX)
+                    dilatedImage_1 = imdilate(ismember(img_sideOfBorderCell, newVerticesActual.verticesConnectCells(numEmptyCell, 1)), strel('disk', numRadius));
+                    dilatedImage_2 = imdilate(ismember(img_sideOfBorderCell, newVerticesActual.verticesConnectCells(numEmptyCell, 2)), strel('disk', numRadius));
+                    dilatedImage_3 = imdilate(ismember(img_sideOfBorderCell, newVerticesActual.verticesConnectCells(numEmptyCell, 3)), strel('disk', numRadius));
+                    
+                    [newVertexX, newVertexY] = find(dilatedImage_1 & dilatedImage_2 & dilatedImage_3);
+                    numRadius = numRadius + 1;
+                end
+                newVertex = round(mean([newVertexX, newVertexY], 1));
+            end
+        end
+
+        newVertices{numSide} = [newVertices{numSide}; vertcat(newVerticesActual.verticesPerCell{neighbourOfBorderCell==0})];
+        newVerticesNeighs2D{numSide} = [newVerticesNeighs2D{numSide}; newVerticesActual.verticesConnectCells(neighbourOfBorderCell==0, :)];
 %         newVertices{numSide} = [newVertices{numSide}; vertcat(newVerticesActual.verticesPerCell{:})];
 %         newVerticesNeighs2D{numSide} = [newVerticesNeighs2D{numSide}; newVerticesActual.verticesConnectCells];
     end
-
+    
     [newVerticesNeighs2D{1}, indicesUnique_1] = unique(newVerticesNeighs2D{1}, 'rows');
     newVertices{1} = newVertices{1}(indicesUnique_1, :);
     [newVerticesNeighs2D{2}, indicesUnique_2] = unique(newVerticesNeighs2D{2}, 'rows');
