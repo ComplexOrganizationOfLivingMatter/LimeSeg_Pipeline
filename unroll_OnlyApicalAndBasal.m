b14a3636b6471ef416546b72140a3132890adff2
Pablo Vicente Munuera <pvicente1@us.es>
2019-04-12 10:10:59 +0200

    Update unroll_OnlyApicalAndBasal.m
    bugfix

--- unroll_OnlyApicalAndBasal.m
+++ unroll_OnlyApicalAndBasal.m
@@ -4,15 +4,14 @@ function unroll_OnlyApicalAndBasal(selpath)
 
     %% ONLY APICAL AND BASAL
     load(fullfile(selpath, '3d_layers_info.mat'), 'labelledImage', 'apicalLayer', 'basalLayer', 'colours', 'glandOrientation', 'lumenImage');
-    load(fullfile(selpath, 'valid_cells.mat'), 'validCells', 'noValidCells');
     
     apicalAreaValidCells = 100;
     disp('Apical');
     mkdir(fullfile(selpath, 'unrolledGlands', 'gland_SR_1'));
-    [~, apicalAreaValidCells] = unrollTube(apicalLayer, fullfile(selpath, 'unrolledGlands', 'gland_SR_1'), noValidCells, colours);
+    [~, apicalAreaValidCells] = unrollTube(apicalLayer, fullfile(selpath, 'unrolledGlands', 'gland_SR_1'), fullfile(selpath, 'valid_cells.mat'), fullfile(selpath, '3d_layers_info.mat'));
 
     disp('Basal');
     mkdir(fullfile(selpath, 'unrolledGlands', 'gland_SR_basal'));
-    unrollTube(basalLayer, fullfile(selpath, 'unrolledGlands', 'gland_SR_basal'), noValidCells, colours, apicalAreaValidCells);
+    unrollTube(basalLayer, fullfile(selpath, 'unrolledGlands', 'gland_SR_basal'), fullfile(selpath, 'valid_cells.mat'), fullfile(selpath, '3d_layers_info.mat'), apicalAreaValidCells);
 end
 
