4ea70f053491a32a475a8f8293d5772a1b4d3149
Pablo Vicente Munuera <pvicente1@us.es>
2019-04-17 10:56:03 +0200

    Update fill0sWithCells.m
    Partitioning Pdist in ranges to improve efficiency

--- lib/fill0sWithCells.m
+++ lib/fill0sWithCells.m
@@ -7,8 +7,25 @@ function [labelMask] = fill0sWithCells(labelMask, invalidRegion)
     [xEdge, yEdge, zEdge] = ind2sub(size(labelMask), edgePixels);
     pixelsIndices = find((imdilate(missingRegions, strel('sphere', 4)).*labelMask)>0);
     [x, y, z] = ind2sub(size(labelMask), pixelsIndices);
-    [~, indices] = pdist2([x, y, z], [xEdge, yEdge, zEdge], 'euclidean', 'Smallest', 1);
-    labelMask(edgePixels) = labelMask(pixelsIndices(indices));
+    
+    numPartitions = 1000;
+    indices = cell(numPartitions, 1);
+    distances = cell(numPartitions, 1);
+    partialPxs = floor(length(x)/numPartitions);
+    for nPart = 1 : numPartitions
+        subIndCoord = (1 + (nPart-1) * partialPxs) : (nPart * partialPxs);
+        if nPart == numPartitions
+            subIndCoord = (1 + (nPart-1) * partialPxs) : length(x);
+        end
+        [distances{nPart}, indices_nPart] = pdist2([x(subIndCoord), y(subIndCoord), z(subIndCoord)], [xEdge, yEdge, zEdge], 'euclidean', 'Smallest', 1);
+        indices{nPart} = subIndCoord(indices_nPart);
+    end
+    distances_all = vertcat(distances{:});
+    indices_all = vertcat(indices{:});
+    
+    [~, indices_Partitions] = min(distances_all);
+    
+    labelMask(edgePixels) = labelMask(pixelsIndices(diag(indices_all(indices_Partitions, 1:length(indices_Partitions)))));
     %figure; paint3D(labelMask);
 end
 
