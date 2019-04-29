7479e5cf739cf32bb00dae81bf0874275569966e
Pablo Vicente Munuera <pvicente1@us.es>
2019-04-29 11:58:27 +0200

    working version
    Co-Authored-By: anttagjan <anttagjan@users.noreply.github.com>

--- lib/normalizeVerticesGland.m
+++ lib/normalizeVerticesGland.m
@@ -1,12 +1,17 @@
-function [samiraTable] = normalizeVerticesGland(samiraTable,deployedImg3x)
+function [samiraTable, glandMinValue] = normalizeVerticesGland(samiraTable, cutValue)
 %NORMALIZEVERTICESGLAND Summary of this function goes here
 %   Detailed explanation goes here
+
 for numRow=1:size(samiraTable,1)
     actualVertices = samiraTable{numRow,5};
-    actualVertices(1:2:end)= actualVertices(1:2:end)- round(size(deployedImg3x,1)/2);
+    actualVertices(1:2:end)= actualVertices(1:2:end) - cutValue;
     samiraTable{numRow,5}=actualVertices;
     minValues(numRow)= min(actualVertices);
+    if cutValue < 0 & minValues(numRow) < 0 
+        disp('error');
+    end
 end 
+
 glandMinValue=min(minValues);
 
 end
--- src/correctSamiraTable.m
+++ src/correctSamiraTable.m
@@ -12,11 +12,20 @@ files = files(nonDiscardedFiles);
 for numFile = 1:length(files)
     files(numFile).folder
     gland_SRs= dir(fullfile(files(numFile).folder, '**', 'samiraTable.mat'));
+    
     for numSR= 1:length(gland_SRs)
         load(fullfile(gland_SRs(numSR).folder, gland_SRs(numSR).name));
         load(fullfile(gland_SRs(numSR).folder, 'allInfo.mat'), 'deployedImg3x');
-        [samiraTable] = normalizeVerticesGland(samiraTable,deployedImg3x);
+        [allSamiraTable{numSR}, glandMinValue(numSR)] = normalizeVerticesGland(samiraTable,round(size(deployedImg3x,1)/2));
     end
     
+    for numSR= 1:length(gland_SRs) 
+        [normalizedSamiraTable{numSR}] = normalizeVerticesGland(allSamiraTable{numSR}, min(glandMinValue));
+    end
+    save(fullfile(gland_SRs(numSR).folder, gland_SRs(numSR).name), 'normalizedSamiraTable', '-append');
     
+    %% Creating samira table
+    samiraTableT = cell2table(normalizedSamiraTable, 'VariableNames',{'Radius', 'CellIDs', 'TipCells', 'BorderCell','verticesValues_x_y'});
+    
+    writetable(samiraTableT, strcat(selpath, '\', idName ,'_samirasFormat.xls'));
 end
