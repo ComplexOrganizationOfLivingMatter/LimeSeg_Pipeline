2c19401857b5e32239d84b09fe76ec7a653481b9
Pablo Vicente Munuera <pvicente1@us.es>
2019-04-29 12:34:24 +0200

    Update correctSamiraTable.m
    export to excel
    
    Co-Authored-By: anttagjan <anttagjan@users.noreply.github.com>

--- src/correctSamiraTable.m
+++ src/correctSamiraTable.m
@@ -5,7 +5,7 @@ addpath(genpath('gui'))
 close all
 clear all
 
-files = dir('**/data/Salivary gland_ToDivideInConstantPieces/**/Results/glandDividedInSurfaceRatios_AllUnrollFeatures.mat');
+files = dir('**/data/Salivary gland_ToDivideInConstantPieces/**/Results/*_samirasFormat.xls');
 
 nonDiscardedFiles = cellfun(@(x) contains(lower(x), 'discarded') == 0 && contains(lower(x), 'wildtype'), {files.folder});
 files = files(nonDiscardedFiles);
@@ -19,13 +19,18 @@ for numFile = 1:length(files)
         [allSamiraTable{numSR}, glandMinValue(numSR)] = normalizeVerticesGland(samiraTable,round(size(deployedImg3x,1)/2));
     end
     
+    load(fullfile(files(numFile).folder,'glandDividedInSurfaceRatios_AllUnrollFeatures.mat'));
+    
     for numSR= 1:length(gland_SRs) 
         [normalizedSamiraTable{numSR}] = normalizeVerticesGland(allSamiraTable{numSR}, min(glandMinValue));
+        actualSamiraTable=normalizedSamiraTable{numSR};
+        actualSamiraTable(:,1)={infoPerSurfaceRatio.SR3D(numSR)};
+        normalizedSamiraTable{numSR} = actualSamiraTable;
     end
     save(fullfile(gland_SRs(numSR).folder, gland_SRs(numSR).name), 'normalizedSamiraTable', '-append');
-    
+       
     %% Creating samira table
+    normalizedSamiraTable = vertcat(normalizedSamiraTable{:});
     samiraTableT = cell2table(normalizedSamiraTable, 'VariableNames',{'Radius', 'CellIDs', 'TipCells', 'BorderCell','verticesValues_x_y'});
-    
-    writetable(samiraTableT, strcat(selpath, '\', idName ,'_samirasFormat.xls'));
+    writetable(samiraTableT, fullfile(files(numFile).folder, files(numFile).name));
 end
