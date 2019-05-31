ea7f9a6ac0414d19ccfd4bb2bf7b9626d325fa52
Pablo Vicente Munuera <pvicente1@us.es>
2019-05-31 16:51:05 +0200

    Update calculateSurfaceRatioMuntant.m
    Obtaining alphaShape per cell to obtain the convex shape

--- lib/calculateSurfaceRatioMuntant.m
+++ lib/calculateSurfaceRatioMuntant.m
@@ -7,12 +7,25 @@ function [] = calculateSurfaceRatioMuntant(basalLayer, apicalLayer, labelledImag
 %   minimum points of the cells intersecting.
     
     %% Get outer basal layer
-%     outerBasalLayer = imclose(basalLayer>0, strel('sphere', 40));
-    [labelledImageBasalInnerLayer, verticesBasalInnerLayer] = obtainLayerByVertices(basalLayer, labelledImage, validCells);
+    neighbours = calculateNeighbours3D(basalLayer, 2, basalLayer == 0);
+    vertices3dBasal = getVertices3D(basalLayer, neighbours.neighbourhood);
+    neighbours = calculateNeighbours3D(apicalLayer, 2, apicalLayer == 0);
+    vertices3dApical = getVertices3D(apicalLayer, neighbours.neighbourhood);
     
-%     outerapicalLayer = imclose(apicalLayer>0, strel('sphere', 40));
-    [labelledImageApicalOutterLayer, verticesApicalOutterLayer] = obtainLayerByVertices(apicalLayer, labelledImage, validCells);
-    
+    labelledImageToCalculateSR = zeros(size(labelledImage));
+    for numCell = 1:max(labelledImage(:))
+        verticesApical = vertcat(vertices3dApical.verticesPerCell{any(ismember(vertices3dApical.verticesConnectCells, numCell), 2)});
+        verticesBasal = vertcat(vertices3dBasal.verticesPerCell{any(ismember(vertices3dBasal.verticesConnectCells, numCell), 2)});
+        
+        plotCell = alphaShape([verticesBasal; verticesApical], 1);
+        plotCell.Alpha = criticalAlpha(plotCell, 'one-region')+10;
+        
+        indicesCell = find(labelledImage == numCell);
+        [possibleIndicesX, possibleIndicesY, possibleIndicesZ] = ind2sub(size(labelledImage), indicesCell);
+        pixelsIn = inShape(plotCell, possibleIndicesX, possibleIndicesY, possibleIndicesZ);
+        labelledImageToCalculateSR(indicesCell(pixelsIn)) = numCell;
+    end
+
     %% Create the cylinder between both surfaces
     load('D:\Pablo\LimeSeg_Pipeline\tmp\sr_info.mat')
     %TODO: REMOVE NO-VALID CELLS AREA
