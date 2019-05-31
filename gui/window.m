c995825dcabe1d2b6ef89e89dbbde4390b80212d
kapy95 <41288942+kapy95@users.noreply.github.com>
2019-05-31 15:19:41 +0200

    Change the lumen of the glands manually

--- gui/window.m
+++ gui/window.m
@@ -127,16 +127,24 @@ if roiMask ~= -1
 
     if sum(newCellRegion(:)) > 0
         insideGland = labelledImage(:,:,selectedZ) > 0;
-        [y, x] = find(newCellRegion & insideGland');
+        if selectCellId > 0
+            [y, x] = find(newCellRegion & insideGland');
+            newIndices = sub2ind(size(labelledImage), x, y, ones(length(x), 1)*selectedZ);
+            labelledImage(newIndices) = selectCellId;
+            labelledImage(lumenImage>0) = 0;
+            %Smooth surface of next and previos Z
+            labelledImage = smoothCellContour3D(labelledImage, selectCellId, (selectedZ-3):(selectedZ+3), lumenImage);
+            
+        else
+            [y, x] = find(newCellRegion);
+            newIndices = sub2ind(size(labelledImage), x, y, ones(length(x), 1)*selectedZ);
+            labelledImage(newIndices) = selectCellId;
+            lumenImage(newIndices) = 1;
+            labelledImage(lumenImage>0) = 0;
+            setappdata(0, 'lumenImage', lumenImage);
+        end
 
-        newIndices = sub2ind(size(labelledImage), x, y, ones(length(x), 1)*selectedZ);
-
-        labelledImage(newIndices) = selectCellId;
-        labelledImage(lumenImage>0) = 0;
-
-        %Smooth surface of next and previos Z
-        labelledImage = smoothCellContour3D(labelledImage, selectCellId, (selectedZ-3):(selectedZ+3), lumenImage);
-
+        
         setappdata(0, 'labelledImageTemp', labelledImage);
         showSelectedCell();
     end
@@ -230,7 +238,7 @@ function decreaseID_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 newValue = getappdata(0, 'cellId')-1;
-if newValue > 0
+if newValue >= 0
     setappdata(0, 'cellId', newValue);
     set(handles.tbCellId,'string',num2str(newValue));
     showSelectedCell();
