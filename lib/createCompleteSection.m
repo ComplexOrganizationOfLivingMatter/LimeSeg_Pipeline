fbe1647327af5ce54aadaeaaed55681c99f86f6c
Pablo Vicente Munuera <pvicente1@us.es>
2019-08-23 12:08:35 +0200

    Update createCompleteSection.m
    Improving perimeter

--- lib/createCompleteSection.m
+++ lib/createCompleteSection.m
@@ -26,48 +26,59 @@ function [filledImage] = createCompleteSection(img3d, coordZ, labelledImage_real
 %                     %imshow(filledImageToTest);
 %                     %imshow(filledImage);
 %                 end
-        [x, y] = find(img3d(:, :, coordZ)>0);
-        coordinates = [x, y];
-        newOrderBoundary = boundary(coordinates, 1);
-        coordinates = coordinates(newOrderBoundary, :);
-
-        userConfig = struct('xy',coordinates, 'showProg',false,'showResult',false);
-        resultStruct = tspo_ga(userConfig);
-        newVertSalesman = coordinates(resultStruct.optRoute, :);
-        newVertSalesman = [newVertSalesman; newVertSalesman(1,:)];
-
-%         for numCoord = 1:(size(newVertSalesman, 1)-1)
-%             plot(newVertSalesman(numCoord:numCoord+1, 2), newVertSalesman(numCoord:numCoord+1, 1))
-%         end
-
-        [distancePxs, idsClosest] = pdist2(coordinates, coordinates, 'euclidean', 'Smallest', 4);
-        coordinatesToUnify = coordinates(distancePxs(4, :) > 1.5, :);
-
-        %                 figure; imshow(filledImage)
+        
         filledImageAux = filledImage>2;
-        for numCoord = 1:size(coordinatesToUnify, 1)
-
-            midVertex = coordinatesToUnify(numCoord, :);
-            midvertexIndex = find(ismember(newVertSalesman, midVertex, 'rows'));
-            if length(midvertexIndex)==1
-                initVertex = newVertSalesman(midvertexIndex-1, :);
-                endVertex = newVertSalesman(midvertexIndex+1, :);
-            else
-                initVertex = newVertSalesman(2, :);
-                endVertex = newVertSalesman(end-1, :);
+        filledImagenew = imfill(filledImageAux | filledImage, 'holes');
+        count = 0;
+        boolean = 1;
+        while boolean && count < 2
+            [x, y] = find(img3d(:, :, coordZ)>0);
+            coordinates = [x, y];
+            if count == 0
+                newOrderBoundary = boundary(coordinates, 1);
+                coordinates = coordinates(newOrderBoundary, :);
             end
 
-            [xnAcum1, ynAcum1] = Drawline3D(initVertex(1), initVertex(2), 0, midVertex(1), midVertex(2), 0);
-            [xnAcum2, ynAcum2] = Drawline3D(midVertex(1), midVertex(2), 0, endVertex(1), endVertex(2), 0);
+            userConfig = struct('xy',coordinates, 'showProg',false,'showResult',false);
+            resultStruct = tspo_ga(userConfig);
+            newVertSalesman = coordinates(resultStruct.optRoute, :);
+            newVertSalesman = [newVertSalesman; newVertSalesman(1,:)];
+% 
+%             for numCoord = 1:(size(newVertSalesman, 1)-1)
+%                 plot(newVertSalesman(numCoord:numCoord+1, 2), newVertSalesman(numCoord:numCoord+1, 1))
+%             end
 
-            xnAcum = [xnAcum1; xnAcum2];
-            ynAcum = [ynAcum1; ynAcum2];
-            %hold on;
-            %plot(coordinatesToUnify(numCoord, 2), coordinatesToUnify(numCoord, 1), 'r*')
-            indicesToSave = sub2ind(size(img3d(:, :, coordZ)), xnAcum, ynAcum);
-            filledImageAux(indicesToSave) = 1;
+            [distancePxs] = pdist2(coordinates, coordinates, 'euclidean', 'Smallest', 4);
+            coordinatesToUnify = coordinates(distancePxs(4, :) > 1, :);
+
+            %                 figure; imshow(filledImage)
+            for numCoord = 1:size(coordinatesToUnify, 1)
+
+                midVertex = coordinatesToUnify(numCoord, :);
+                midvertexIndex = find(ismember(newVertSalesman, midVertex, 'rows'));
+                if length(midvertexIndex)==1
+                    initVertex = newVertSalesman(midvertexIndex-1, :);
+                    endVertex = newVertSalesman(midvertexIndex+1, :);
+                else
+                    initVertex = newVertSalesman(2, :);
+                    endVertex = newVertSalesman(end-1, :);
+                end
+
+                [xnAcum1, ynAcum1] = Drawline3D(initVertex(1), initVertex(2), 0, midVertex(1), midVertex(2), 0);
+                [xnAcum2, ynAcum2] = Drawline3D(midVertex(1), midVertex(2), 0, endVertex(1), endVertex(2), 0);
+
+                xnAcum = [xnAcum1; xnAcum2];
+                ynAcum = [ynAcum1; ynAcum2];
+                %hold on;
+                %plot(coordinatesToUnify(numCoord, 2), coordinatesToUnify(numCoord, 1), 'r*')
+                indicesToSave = sub2ind(size(img3d(:, :, coordZ)), xnAcum, ynAcum);
+                filledImageAux(indicesToSave) = 1;
+            end
+            filledImagenew = imfill(filledImageAux | filledImage, 'holes');
+            count = count + 1;
+            boolean = max(max(bwlabel(filledImagenew - filledImage))) > 2;
         end
-        filledImagenew = imfill(filledImageAux | filledImage, 'holes');
+        
         if isequal(filledImage, filledImagenew) == 0
             filledImage = filledImagenew;
             filledImage = bwareafilt(filledImage>0, 1, 4);
