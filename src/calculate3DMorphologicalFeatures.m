8b618c02f607be375b8f108ec25c64620af8ebf1
Pablo Vicente Munuera <pvicente1@us.es>
2019-08-30 12:32:31 +0200

    Update calculate3DMorphologicalFeatures.m
    Format and output file changed

--- src/calculate3DMorphologicalFeatures.m
+++ src/calculate3DMorphologicalFeatures.m
@@ -1,7 +1,8 @@
-function [outputArg1,outputArg2] = calculate3DMorphologicalFeatures(inputArg1,inputArg2)
+function [] = calculate3DMorphologicalFeatures(folderName)
 %CALCULATE3DMORPHOLOGICALFEATURES Summary of this function goes here
 %   Detailed explanation goes here
-files = dir('**/data/Salivary gland/**/Results/3d_layers_info.mat');
+
+files = dir(fullfile('**/data/Salivary gland/', folderName, '/**/Results/3d_layers_info.mat'));
 nonDiscardedFiles = cellfun(@(x) contains(lower(x), 'discarded') == 0, {files.folder});
 files = files(nonDiscardedFiles);
 
@@ -12,42 +13,42 @@ allLumens = [];
 allFilesName = [];
 
 for numFiles=1:length(files)
-load(fullfile(files(numFiles).folder, '3d_layers_info.mat'), 'labelledImage_realSize', 'lumenImage_realSize'); 
-load(fullfile(files(numFiles).folder, 'valid_cells.mat'), 'validCells'); 
-
-%% Extract each cell and calculate 3D features        
-[cells3dFeatures] = extract3dDescriptors(labelledImage_realSize, validCells');
-
-%% Lumen features
-[lumen3dFeatures] = extract3dDescriptors(lumenImage_realSize, 1);
-lumen3dFeatures.ID_Cell = 'Lumen';
-
-%% Global Gland
-% We need calculate thickness of the glands or number of cell in
-% transversal axis
-[gland3dFeatures] = extract3dDescriptors(labelledImage_realSize>0, 1);
-gland3dFeatures.ID_Cell = 'Gland';
-
-allFeatures = vertcat(cells3dFeatures, gland3dFeatures, lumen3dFeatures);
-%% Save variables and export to excel
-writetable(allFeatures,fullfile(files(numFiles).folder,'3dFeatures_LimeSeg3DSegmentation.xls'), 'Range','B2');
-save(fullfile(files(numFiles).folder, 'morphological3dFeatures.mat'), 'allFeatures') 
-
-%% Calculate mean and std of 3D features
-meanFeatures = varfun(@(x) mean(x),cells3dFeatures(2:end, :));
-stdFeatures = varfun(@(x) std(x),cells3dFeatures(2:end, :));
-
-totalMeanFeatures(end+1) = meanFeatures;
-totalStdFeatures(end+1) = stdFeatures;
-allGlands(end+1) = gland3dFeatures;
-allLumens(end+1) = lumen3dFeatures;
-
-fileName = strsplit(files(numFiles).folder, {'/','\'});
-fileName = convertCharsToStrings(strjoin({fileName{1,end-2},fileName{1,end-1}}, ' '));
-allFilesName = [allFilesName ; fileName];
+    load(fullfile(files(numFiles).folder, '3d_layers_info.mat'), 'labelledImage_realSize', 'lumenImage_realSize');
+    load(fullfile(files(numFiles).folder, 'valid_cells.mat'), 'validCells');
+    
+    %% Extract each cell and calculate 3D features
+    [cells3dFeatures] = extract3dDescriptors(labelledImage_realSize, validCells');
+    
+    %% Lumen features
+    [lumen3dFeatures] = extract3dDescriptors(lumenImage_realSize, 1);
+    lumen3dFeatures.ID_Cell = 'Lumen';
+    
+    %% Global Gland
+    % We need calculate thickness of the glands or number of cell in
+    % transversal axis
+    [gland3dFeatures] = extract3dDescriptors(labelledImage_realSize>0, 1);
+    gland3dFeatures.ID_Cell = 'Gland';
+    
+    allFeatures = vertcat(cells3dFeatures, gland3dFeatures, lumen3dFeatures);
+    %% Save variables and export to excel
+    writetable(allFeatures,fullfile(files(numFiles).folder,'3dFeatures_LimeSeg3DSegmentation.xls'), 'Range','B2');
+    save(fullfile(files(numFiles).folder, 'morphological3dFeatures.mat'), 'allFeatures')
+    
+    %% Calculate mean and std of 3D features
+    meanFeatures = varfun(@(x) mean(x),cells3dFeatures(2:end, :));
+    stdFeatures = varfun(@(x) std(x),cells3dFeatures(2:end, :));
+    
+    totalMeanFeatures(end+1) = meanFeatures;
+    totalStdFeatures(end+1) = stdFeatures;
+    allGlands(end+1) = gland3dFeatures;
+    allLumens(end+1) = lumen3dFeatures;
+    
+    fileName = strsplit(files(numFiles).folder, {'/','\'});
+    fileName = convertCharsToStrings(strjoin({fileName{1,end-2},fileName{1,end-1}}, ' '));
+    allFilesName = [allFilesName ; fileName];
 end
 
-selpath = dir('**/data/Salivary gland/');
+selpath = dir(fullfile('**/data/Salivary gland/', folderName));
 
 allFilesName = table(allFilesName, 'VariableNames', {'ID_Glands'});
 totalStdFeatures.Properties.VariableNames = {'stdVolume','stdEquivDiameter','stdPrincipalAxisLength','stdConvexVolume', 'stdSolidity', 'stdSurfaceArea', 'stdAspectRatio', 'stdSphericity', 'stdNormalizedVolume'};
