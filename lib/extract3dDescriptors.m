52c34172d4f0d875a745e97e5d0370552f1596e0
Pablo Vicente Munuera <pvicente1@us.es>
2019-12-11 11:27:13 +0100

    Update extract3dDescriptors.m
    We have fixed several issues:
    - Convex volume of regionprops3 was not correctly computed. We have by using delaunay triangulations and convexhull function.
    - Normalize volume was performed wrongly. It was amended also.

--- lib/extract3dDescriptors.m
+++ lib/extract3dDescriptors.m
@@ -4,14 +4,27 @@ function [cells3dFeatures] = extract3dDescriptors(labelledImage, validCells)
 cells3dFeatures=[];
 for indexCell= 1: max(max(max(labelledImage)))   
     oneCell3dFeatures = regionprops3(labelledImage==indexCell, 'PrincipalAxisLength', 'Volume', 'ConvexVolume', 'Solidity', 'SurfaceArea', 'EquivDiameter');
+    
+    indMax = 1;
+    if size(oneCell3dFeatures, 1) > 1
+        [~,indMax] = max(oneCell3dFeatures.Volume);
+        oneCell3dFeatures = oneCell3dFeatures(indMax,:);
+    end
+    actualImg = bwlabeln(labelledImage==indexCell);
+    [x, y, z] = ind2sub(size(labelledImage), find(actualImg==indMax));
+    DT = delaunayTriangulation(x, y, z);
+    [~, convexVolume] = convexHull(DT);
+    oneCell3dFeatures.ConvexVolume = convexVolume;
+    oneCell3dFeatures.Solidity = sum(actualImg(:)==indMax) / convexVolume;
     aspectRatio = max(oneCell3dFeatures.PrincipalAxisLength,[],2) ./ min(oneCell3dFeatures.PrincipalAxisLength,[],2);
     sphereArea = 4 * pi .* ((oneCell3dFeatures.EquivDiameter) ./ 2) .^ 2;
     sphericity = sphereArea ./ oneCell3dFeatures.SurfaceArea;
-    normalizedVolume = oneCell3dFeatures.Volume/mean(oneCell3dFeatures.Volume);
-    oneCell3dFeatures = horzcat(oneCell3dFeatures, table(aspectRatio, sphericity, normalizedVolume));
-    [~,indMax] = max(oneCell3dFeatures.Volume);
-    cells3dFeatures = [cells3dFeatures; oneCell3dFeatures(indMax,:)];
+    normalizedVolume = oneCell3dFeatures.Volume;
+    
+    cells3dFeatures = [cells3dFeatures; horzcat(oneCell3dFeatures, table(aspectRatio, sphericity, normalizedVolume))];
 end
+cells3dFeatures.normalizedVolume = arrayfun(@(x) x/mean(cells3dFeatures.Volume), cells3dFeatures.normalizedVolume);
+
 columnIDs = table('Size', size(validCells), 'VariableTypes', {'string'});
 columnIDs.Properties.VariableNames = {'ID_Cell'};
 columnIDs.ID_Cell = arrayfun(@(x) strcat('cell_', num2str(x)), validCells, 'UniformOutput', false);
