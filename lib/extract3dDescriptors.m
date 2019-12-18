e08ba1e7717706993a5e72b23472f2b2df74a34f
Pablo Vicente Munuera <pvicente1@us.es>
2019-12-18 10:06:41 +0100

    Bugfix: When a cell is not found

--- lib/extract3dDescriptors.m
+++ lib/extract3dDescriptors.m
@@ -4,24 +4,37 @@ function [cells3dFeatures] = extract3dDescriptors(labelledImage, validCells)
 cells3dFeatures=[];
 for indexCell= 1: max(max(max(labelledImage)))   
     oneCell3dFeatures = regionprops3(labelledImage==indexCell, 'PrincipalAxisLength', 'Volume', 'ConvexVolume', 'Solidity', 'SurfaceArea', 'EquivDiameter');
-    
-    indMax = 1;
-    if size(oneCell3dFeatures, 1) > 1
-        [~,indMax] = max(oneCell3dFeatures.Volume);
-        oneCell3dFeatures = oneCell3dFeatures(indMax,:);
+    if size(oneCell3dFeatures, 1) > 0
+        indMax = 1;
+        if size(oneCell3dFeatures, 1) > 1
+            [~,indMax] = max(oneCell3dFeatures.Volume);
+            oneCell3dFeatures = oneCell3dFeatures(indMax,:);
+        end
+        actualImg = bwlabeln(labelledImage==indexCell);
+        [x, y, z] = ind2sub(size(labelledImage), find(actualImg==indMax));
+        DT = delaunayTriangulation(x, y, z);
+        [~, convexVolume] = convexHull(DT);
+        oneCell3dFeatures.ConvexVolume = convexVolume;
+        oneCell3dFeatures.Solidity = sum(actualImg(:)==indMax) / convexVolume;
+        aspectRatio = max(oneCell3dFeatures.PrincipalAxisLength,[],2) ./ min(oneCell3dFeatures.PrincipalAxisLength,[],2);
+        sphereArea = 4 * pi .* ((oneCell3dFeatures.EquivDiameter) ./ 2) .^ 2;
+        sphericity = sphereArea ./ oneCell3dFeatures.SurfaceArea;
+        normalizedVolume = oneCell3dFeatures.Volume;
+        cells3dFeatures = [cells3dFeatures; horzcat(oneCell3dFeatures, table(aspectRatio, sphericity, normalizedVolume))];
+    else
+        badOneCell = cells3dFeatures(end, :);
+       	badOneCell.PrincipalAxisLength(1,1:3) = -1;
+        badOneCell.Volume = -1;
+        badOneCell.aspectRatio = -1;
+        badOneCell.ConvexVolume = -1;
+        badOneCell.EquivDiameter = -1;
+        badOneCell.Solidity = -1;
+        badOneCell.normalizedVolume = -1;
+        badOneCell.sphericity = -1;
+        badOneCell.SurfaceArea = -1;
+        badOneCell.Volume = -1;
+        cells3dFeatures = [cells3dFeatures; badOneCell];
     end
-    actualImg = bwlabeln(labelledImage==indexCell);
-    [x, y, z] = ind2sub(size(labelledImage), find(actualImg==indMax));
-    DT = delaunayTriangulation(x, y, z);
-    [~, convexVolume] = convexHull(DT);
-    oneCell3dFeatures.ConvexVolume = convexVolume;
-    oneCell3dFeatures.Solidity = sum(actualImg(:)==indMax) / convexVolume;
-    aspectRatio = max(oneCell3dFeatures.PrincipalAxisLength,[],2) ./ min(oneCell3dFeatures.PrincipalAxisLength,[],2);
-    sphereArea = 4 * pi .* ((oneCell3dFeatures.EquivDiameter) ./ 2) .^ 2;
-    sphericity = sphereArea ./ oneCell3dFeatures.SurfaceArea;
-    normalizedVolume = oneCell3dFeatures.Volume;
-
-    cells3dFeatures = [cells3dFeatures; horzcat(oneCell3dFeatures, table(aspectRatio, sphericity, normalizedVolume))];
 end
 cells3dFeatures.normalizedVolume = arrayfun(@(x) x/mean(cells3dFeatures.Volume), cells3dFeatures.normalizedVolume);
 
