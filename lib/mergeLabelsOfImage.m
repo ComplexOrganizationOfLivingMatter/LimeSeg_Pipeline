879c2545ccc05a65836a3621088de1bec5179eaa
Pablo Vicente Munuera <pvicente1@us.es>
2020-03-09 14:26:26 +0100

    Working merge

--- lib/mergeLabelsOfImage.m
+++ lib/mergeLabelsOfImage.m
@@ -2,7 +2,7 @@ function [labelledImage] = mergeLabelsOfImage(labelledImage, cellsToMerge)
 %MERGELABELSOFIMAGE Summary of this function goes here
 %   Detailed explanation goes here
     for numCellsToMerge = 2:length(cellsToMerge)
-        labelledImage(cellsToMerge(numCellsToMergemain)) = cellsToMerge(1);
+        labelledImage(labelledImage == cellsToMerge(numCellsToMerge)) = cellsToMerge(1);
     end
 end
 
