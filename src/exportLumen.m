8e18e4a762d3fc447afc4269947c331495393c3a
Pablo Vicente Munuera <pvicente1@us.es>
2020-03-12 16:09:11 +0100

    Corrrecting lumen

--- src/exportLumen.m
+++ src/exportLumen.m
@@ -1,14 +1,9 @@
 function exportLumen(lumenImage, outputDir, tipValue)
 %EXPORTASIMAGESEQUENCE Summary of this function goes here
 %   Detailed explanation goes here
-
     mkdir(fullfile(outputDir, 'Lumen', 'inferLumen'));        
-  
     for numZ = 1:(size(lumenImage, 3))
-        ImgWithoutTips =  lumenImage;
-        actualImg = imcomplement(imresize(ImgWithoutTips',[1024,1024], 'nearest'));
-        
-        imwrite(double(actualImg), fullfile(outputDir,'Lumen/inferLumen', strcat('/lumenImage_', num2str([numZ-(tipValue+1)].','%03d'), '.tif')))
+        actualImg = imcomplement(lumenImage(:, :, numZ));
+        imwrite(double(actualImg), fullfile(outputDir,'Lumen/inferLumen', strcat('/lumenImage_', num2str([numZ].','%03d'), '.tif')))
     end
-    
 end
