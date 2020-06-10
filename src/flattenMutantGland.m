7162dffe3107b4580cc1c666721e136ea87ad8c3
pedgomgal1 <pgomez-ibis@us.es>
2020-06-10 11:11:09 +0200

    adjust alphaShape for flatten glands

--- src/flattenMutantGland.m
+++ src/flattenMutantGland.m
@@ -8,17 +8,23 @@ function [labelledImageRealSR] = flattenMutantGland(apicalLayer, basalLayer, lab
     vertices3dApical = getVertices3D(apicalLayer, neighbours.neighbourhood);
     
     labelledImageToCalculateSR = zeros(size(labelledImage));
-    for numCell = 1:max(labelledImage(:))
+    uniqCells = unique(labelledImage(:))';
+    for numCell = uniqCells(2:end)
         verticesApical = vertcat(vertices3dApical.verticesPerCell{any(ismember(vertices3dApical.verticesConnectCells, numCell), 2)});
         verticesBasal = vertcat(vertices3dBasal.verticesPerCell{any(ismember(vertices3dBasal.verticesConnectCells, numCell), 2)});
-        
-        plotCell = alphaShape([verticesBasal; verticesApical], 1);
-        plotCell.Alpha = criticalAlpha(plotCell, 'one-region')+10;
-        
-        indicesCell = find(labelledImage == numCell);
-        [possibleIndicesX, possibleIndicesY, possibleIndicesZ] = ind2sub(size(labelledImage), indicesCell);
-        pixelsIn = inShape(plotCell, possibleIndicesX, possibleIndicesY, possibleIndicesZ);
-        labelledImageToCalculateSR(indicesCell(pixelsIn)) = numCell;
+        try 
+            plotCell = alphaShape([verticesBasal; verticesApical], 1);
+            plotCell.Alpha = criticalAlpha(plotCell, 'one-region')+20;
+
+            indicesCell = find(labelledImage == numCell);
+            [possibleIndicesX, possibleIndicesY, possibleIndicesZ] = ind2sub(size(labelledImage), indicesCell);
+            pixelsIn = inShape(plotCell, possibleIndicesX, possibleIndicesY, possibleIndicesZ);
+            labelledImageToCalculateSR(indicesCell(pixelsIn)) = numCell;
+        catch
+            %try/catch for non-valid cells with strange shape (avoid flatten)
+            print(['Cell ' num2str(numCell) ' without flatten'])
+            labelledImageToCalculateSR(labelledImage==numCell)=numCell;
+        end
     end 
     labelledImageRealSR = double(imclose(labelledImageToCalculateSR > 0,strel('sphere',2)));
     labelledImageRealSR = labelledImageRealSR .* labelledImage;
